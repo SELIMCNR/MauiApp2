dc46e52f8cb2c81a4b4f4b922c1b6c218e9ccb3b
Selim Çınar <btk02672@gmail.com>
2024-12-10 22:55:14 +0300

    first commit

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private async void DrawingView_DrawingLineCompleted(object sender,
+            CommunityToolkit.Maui.Core.DrawingLineCompletedEventArgs e)
+        {
+
+            var stream = await myDrawing.GetImageStream(150,150);
+            myImage.Source = ImageSource.FromStream(() => stream);
+            
+
+
+        }
+    }
+
+}
